42df2a73e1117f96bc1b7d81da43e0a1472c780c
nzy1997 <zni573@connect.hkust-gz.edu.cn>
2026-05-12 11:58:19 +0800

    feat: add sample trace types

---
+++ rstim/src/sample_trace.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SampleTrace {
+    pub noise_events: Vec<NoiseEvent>,
+    pub measurement_events: Vec<MeasurementEvent>,
+    pub detector_events: Vec<DetectorEvent>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct NoiseEvent {
+    pub op_path: Vec<usize>,
+    pub repeat_iterations: Vec<usize>,
+    pub instr_name: String,
+    pub target_slots: Vec<usize>,
+    pub target_qubits: Vec<u32>,
+    pub occurred: bool,
+    pub branch_label: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MeasurementEvent {
+    pub op_path: Vec<usize>,
+    pub repeat_iterations: Vec<usize>,
+    pub target_slot: usize,
+    pub target_qubit: u32,
+    pub instr_name: String,
+    pub measurement_index: usize,
+    pub bit: bool,
+    pub loss_cause: bool,
+    pub component: MeasurementComponent,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DetectorEvent {
+    pub op_path: Vec<usize>,
+    pub repeat_iterations: Vec<usize>,
+    pub detector_index: usize,
+    pub flipped: bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum MeasurementComponent {
+    Value,
+    LossFlag,
+}
---
+++ rstim/tests/sample_trace.rs
@@ -0,0 +1,39 @@
+use rstim::sample_trace::{
+    DetectorEvent, MeasurementComponent, MeasurementEvent, NoiseEvent, SampleTrace,
+};
+
+#[test]
+fn sample_trace_holds_noise_measurement_and_detector_events() {
+    let trace = SampleTrace {
+        noise_events: vec![NoiseEvent {
+            op_path: vec![1],
+            repeat_iterations: vec![0],
+            instr_name: "DEPOLARIZE1".to_string(),
+            target_slots: vec![0],
+            target_qubits: vec![5],
+            occurred: true,
+            branch_label: Some("Y".to_string()),
+        }],
+        measurement_events: vec![MeasurementEvent {
+            op_path: vec![2],
+            repeat_iterations: vec![0],
+            target_slot: 0,
+            target_qubit: 5,
+            instr_name: "M".to_string(),
+            measurement_index: 1,
+            bit: true,
+            loss_cause: false,
+            component: MeasurementComponent::Value,
+        }],
+        detector_events: vec![DetectorEvent {
+            op_path: vec![3],
+            repeat_iterations: vec![0],
+            detector_index: 0,
+            flipped: true,
+        }],
+    };
+
+    assert_eq!(trace.noise_events[0].branch_label.as_deref(), Some("Y"));
+    assert!(trace.measurement_events[0].bit);
+    assert!(trace.detector_events[0].flipped);
+}
