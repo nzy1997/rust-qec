33de33ab889d40ba55ffecaed07136983abd9764
nzy1997 <zni573@connect.hkust-gz.edu.cn>
2026-06-16 16:59:12 +0800

    feat: add built-in css code spec selector

--- qec-code/src/codes/built_in_css.rs
+++ qec-code/src/codes/built_in_css.rs
@@ -8,6 +8,59 @@ pub struct BuiltInCssChecks {
     pub hz: Vec<Vec<usize>>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BuiltInCssCodeSpec {
+    Fixed {
+        code_id: &'static str,
+    },
+    Family {
+        family: BuiltInCssFamily,
+        params: BuiltInCssParams,
+    },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BuiltInCssFamily {
+    RepetitionX,
+    RepetitionZ,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct BuiltInCssParams {
+    pub distance: usize,
+}
+
+pub fn parse_built_in_css_code_spec(input: &str) -> Result<BuiltInCssCodeSpec> {
+    if let Some((family_name, params_text)) = input.split_once(':') {
+        let family = match family_name {
+            "repetition_x" => BuiltInCssFamily::RepetitionX,
+            "repetition_z" => BuiltInCssFamily::RepetitionZ,
+            _ => {
+                return Err(QecError::UnknownBuiltInCssCode {
+                    code_id: family_name.to_owned(),
+                });
+            }
+        };
+
+        let distance = params_text
+            .strip_prefix("d=")
+            .and_then(|value| value.parse::<usize>().ok())
+            .unwrap_or(0);
+
+        return Ok(BuiltInCssCodeSpec::Family {
+            family,
+            params: BuiltInCssParams { distance },
+        });
+    }
+
+    match input {
+        "steane" => Ok(BuiltInCssCodeSpec::Fixed { code_id: "steane" }),
+        _ => Err(QecError::UnknownBuiltInCssCode {
+            code_id: input.to_owned(),
+        }),
+    }
+}
+
 const STEANE_ROW_SUPPORTS: &[&[usize]] = &[
     &[0, 3, 5, 6],
     &[1, 3, 4, 6],
