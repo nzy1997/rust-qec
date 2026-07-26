86782c23ef3af422abc2ba5dadf13960418f90f1
nzy1997 <zni573@connect.hkust-gz.edu.cn>
2026-07-26 23:33:32 +0800

    style: format qec-code error enum

--- qec-code/src/error.rs
+++ qec-code/src/error.rs
@@ -181,7 +181,9 @@ pub enum QecError {
     },
     #[error("unexpected built-in CSS parameter {parameter} for family {family}")]
     UnexpectedBuiltInCssParameter { family: String, parameter: String },
-    #[error("out-of-range built-in CSS integer parameter {parameter} for family {family}: {value}")]
+    #[error(
+        "out-of-range built-in CSS integer parameter {parameter} for family {family}: {value}"
+    )]
     OutOfRangeBuiltInCssIntegerParameter {
         family: String,
         parameter: String,
